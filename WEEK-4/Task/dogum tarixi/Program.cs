aa1b8eacc933274830d516d37f2f99d088f4c342
Resad Memmedov <talehg2932@gmail.com>
2021-04-10 00:19:42 +0400

    Task-4 un yekunlasdirilmasi

---
+++ WEEK-4/Task/dogum tarixi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dogum_tarixi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 6. yas hesablanmasi
+            /*
+             * dogum tarixini daxil eden istifedecinin
+             * yasini hesablayib ekrana cap edin
+             */
+            #endregion
+            #region
+            Console.WriteLine("dogum tarixinizi daxil edin: { gun/ay/il } meselen:   15/10/2001     seklinde daxil edin");
+            string arrayElement = Console.ReadLine();
+            var Array = arrayElement.Split("/");
+            int il = DateTime.Now.Year;
+            double age = il - int.Parse(Array[2]);
+            Console.WriteLine($"sizin hazirki yasiniz {age} ");
+            #endregion
+
+        }
+    }
+}
---
+++ WEEK-4/Task/pul el cirki/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pul_el_cirki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            /*
+            * istifadeci bankomatdan cixarmaq istediyi meblegi daxil edir(sonsuz sayda, emeliyyati dayandirmagida nezere alin).
+            * daxil edilen meblege uygun console-a eskinaslari sayi ile cap edirik.
+            *
+            * eskinaslar : 200, 100, 50, 20, 10, 5, 1
+            *
+            * meselen daxil edilen pul miqdari 45 dir:
+            * 2 eded 20 azn
+            * 1 eded 5 azn
+            *
+            */
+            #endregion
+            int[] Array = { 200, 100, 50, 20, 10, 5, 1 };
+            while (true)
+            {
+                Console.WriteLine("Cixarmaq istdeyiniz meblegi daxil edin :");
+                int miqdar = int.Parse(Console.ReadLine());
+                if (miqdar > 0)
+                {
+                    for(int i=0; i < Array.Length; i++)
+                    {
+                        if (miqdar > Array[i])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                          int manat= miqdar / Array[i];
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
