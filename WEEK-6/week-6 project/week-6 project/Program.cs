0484ef01338acfe89c60f4d6bccafd41760aea29
Resad Memmedov <talehg2932@gmail.com>
2021-04-30 17:09:24 +0400

    Week-6 taskin hell olunmasi

---
+++ WEEK-6/week-6 project/week-6 project/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace week_6_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("uzunlugu daxil edin : 1,2,3 .......");
+            rectangle.Uzunluq = int.Parse(Console.ReadLine());
+            Console.WriteLine("en daxil edin :1,2,3 .......");
+            rectangle.En = int.Parse(Console.ReadLine());
+
+            if(rectangle.Uzunluq <=0 || rectangle.En <=0)
+            {
+                Console.WriteLine("Duzbucaqlinin sahesi  0 -di");
+            }
+            else
+            {
+                Console.WriteLine($"Duzbucaqlinin sahesi : {rectangle.Hasil}-dir");
+            }
+        }
+    }
+}
---
+++ WEEK-6/week-6 project/week-6 project/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week_6_project
+{
+    /// <summary>
+    /// Duzbucaqlinin hasilini hesablayan Class
+    /// </summary>
+    class Rectangle
+    {
+        private int en;
+        private int uzunluq;
+        public int En
+        {
+            get
+            {
+                return en;
+            }
+            set
+            {
+                en = value;
+            }
+        }
+        public int Uzunluq
+        {
+            get
+            {
+                return uzunluq;
+            }
+            set
+            {
+                uzunluq = value;
+            }
+        }
+        public double Hasil=>en*uzunluq;
+    }
+}
