8768c77cd50cca7593344ef84f24f0701b1e3e79
Resad Memmedov <talehg2932@gmail.com>
2021-04-30 22:13:11 +0400

    Task-2 nin elave olunmasi

--- WEEK-6/week-6 project/week-6 project/Program.cs
+++ WEEK-6/week-6 project/week-6 project/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace week_6_project
 {
@@ -6,6 +7,16 @@ namespace week_6_project
     {
         static void Main(string[] args)
         {
+            #region Task-1
+            /*
+             * Console-dan daxil edilen en ve uzunluga gore duzbucaqlinin sahesinin hesablanmasi.
+             * class memeber-ler ve encapsulation movuzlarina uygun isleyin.
+             * Qeyd: daxil edilnlerden her hansisa birinin menfi olub olmamasini yoxlayin.
+             * menfidirse deyeri 0 beraber edin.
+             */
+            #endregion
+            #region
+            /*
             Rectangle rectangle = new Rectangle();
 
             Console.WriteLine("uzunlugu daxil edin : 1,2,3 .......");
@@ -21,6 +32,63 @@ namespace week_6_project
             {
                 Console.WriteLine($"Duzbucaqlinin sahesi : {rectangle.Hasil}-dir");
             }
+            */
+            #endregion
+
+
+
+
+
+            #region Task-2
+            /*
+             *  Muellim modeli yaradin, id, adi,soyadi,vezifesi,dogum tarixi, ise baslama tarixi, islediyi yer
+             *  datalarini saxlasin.
+             *  Id: sadece oxuna biler auto yaradilacaq.
+             *  Vezife: daxil edilmeyibse nulldursa Teacher deye set edilsin.
+             *  Ise Baslama tarixi : Muellimin eger 23 yasi varsa muellim olaraq fealiyyet gostere biler.
+             */
+            #endregion
+
+            #region
+            /*
+            Teacher teacher = new Teacher();
+
+            Hashtable muellimData = new Hashtable();
+            ArrayList muellimler = new ArrayList();
+            int yas = DateTime.Now.Year - (teacher.DogumTarixi.Year);
+                Console.WriteLine("adinizi daxil edin :");
+                teacher.Name = Console.ReadLine();
+                Console.WriteLine("Soyadinizi daxil edin :");
+                teacher.Surame = Console.ReadLine();
+                Console.WriteLine("Dogum tarixinizi  daxil edin :");
+                teacher.DogumTarixi=DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Isebaslama tarixini  daxil edin :");
+                teacher.IseBaslamasi = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Is yerini  daxil edin :");
+                teacher.IslediyiYer = Console.ReadLine();
+                if (yas >= 23)
+                {
+                    muellimData.Add("Id", teacher.Id);
+                    muellimData.Add("Ad", teacher.Name);
+                    muellimData.Add("Soyad", teacher.Surame);
+                    muellimData.Add("Vezife", teacher.Vezife);
+                    muellimData.Add("DogumTarixi", teacher.DogumTarixi);
+                    muellimData.Add("IseBaslama", teacher.IseBaslamasi);
+                    muellimData.Add("IsYeri", teacher.IslediyiYer);
+
+                    muellimler.Add(muellimData);
+
+                }
+                 else
+                 {
+                Console.WriteLine("Yasiniz 23-den yuksek olmalidir !");
+                 }
+                foreach (DictionaryEntry items in muellimData)
+                {
+                    Console.WriteLine($"{items.Key} {items.Value}");
+                }
+            */ 
+            #endregion
         }
     }
 }
---
+++ WEEK-6/week-6 project/week-6 project/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace week_6_project
+{
+    /// <summary>
+    /// muellime aid datalar istehsal edir
+    /// </summary>
+    class Teacher
+    {
+        Hashtable muellimData = new Hashtable();
+        private int id=1;
+        private string teacher="Teacher";
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                for (int i = 0; i <10; i++)
+                {
+                    id += 1;
+                }
+            }
+        }
+        public string Name { get; set; }
+        public string Surame { get; set; }
+        public ref readonly  string Vezife =>ref teacher;
+        public DateTime DogumTarixi { get; set; }
+        public DateTime IseBaslamasi { get; set; }
+        public string IslediyiYer { get; set; }
+
+    }
+}
