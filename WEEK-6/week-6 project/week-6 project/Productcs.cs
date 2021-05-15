ef0f54c5b08bf50816733e3a12c50a2fdf0038a8
Resad Memmedov <talehg2932@gmail.com>
2021-05-15 12:35:28 +0400

    week-6 task-3 un bitirilmesi

---
+++ WEEK-6/week-6 project/week-6 project/Productcs.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace week_6_project
+{/// <summary>
+/// Product istehsal edir
+/// </summary>
+    class Productcs
+    {
+        
+        public static double result;
+        public static double total;
+        private static double mebleg;
+
+        private string bravokart="Yoxdur";
+        private string odenis = "Negd";
+        public string Odenis
+        {
+            get
+            {
+                return odenis;
+            }
+            set
+            {
+                if (OdenisNovu(value))
+                {
+                    Hesabla();
+                    mebleg=result- (this.Miqdar * this.Qiymet) *this.Edv*0.15;
+                }
+                else
+                {
+                    Hesabla();
+                    mebleg = result - (this.Miqdar * this.Qiymet) *this.Edv * 0.1;
+                }
+            }
+        }
+        public string BravoKart
+        {
+            get
+            {
+                return bravokart;
+            }
+            set
+            {
+                    if (BravoCart(value))
+                    {
+                    bravokart = value;
+                    Hesabla();
+                    total = mebleg - mebleg * 0.02;
+                    
+                }
+                else
+                {
+                    Hesabla();
+                    total = mebleg;
+                }
+            }
+        }
+        public string Name { get; set; }
+        public int Miqdar  { get; set; }
+        public double Qiymet { get; set; }
+        public double Edv { get; set; } = 0;
+
+        #region Bu method bravoKartin olub olmamasini yoxlayir
+        private static bool BravoCart(string deyer)
+        {
+            if (deyer.ToUpper() == "VAR")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Bu method mehsul ucun odenilecek meblegi hesablayir
+        private void Hesabla()
+        {
+            if (Edv > 0)
+            {
+                result = (this.Miqdar * this.Qiymet) + (this.Miqdar * this.Qiymet) * this.Edv;
+            }
+            else
+            {
+                result = (this.Miqdar * this.Qiymet);
+            }
+        }
+        private static bool OdenisNovu(string odenis)
+        {
+            if (odenis.ToUpper() == "Negd")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        #endregion
+    }
+}
--- WEEK-6/week-6 project/week-6 project/Program.cs
+++ WEEK-6/week-6 project/week-6 project/Program.cs
@@ -87,7 +87,133 @@ namespace week_6_project
                 {
                     Console.WriteLine($"{items.Key} {items.Value}");
                 }
-            */ 
+            */
+            #endregion
+            #region gelende eve corey al
+            /*
+             *  Online alis veris :
+             *  * Console ekaraninda musterini ilk once  menu qarsilamalidir,
+             *  mehsullari menudan sececek( qiymetleri de olmalidir) sonra hansindan ne qeder istediyini qeyd edecek
+             *  bildiklerinizi tetbiq ederek asagidaki numune cekin hazilanmasi
+             *  lazimdir. Mehsul adlari onlarin qiymetleri edv olub olmamasi mehsulda endirim olub olmamasi
+             *  siz terefden serbest teyin olunacaq.(burada gosterilenler hayel urunudur hic biri gercegi yansitmamaktadir).
+             *
+             *  | mehsul adi        |  miqdar  |  qiymet  |EDV  |toplam|
+             *  | Un                | 5 kq     |  1       |0 %  |5     |
+             *   Sizin qazanciniz=================== 0,495             |
+             *  | quzu eti          | 3,5 kq   |  12      |18 % |42    |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | cay               | 2   eded |  1,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | alma qirmizi      | 2   kq   |  2,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | alma sari         | 2   kq   |  2       |18 % |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | goyerti           | 2   eded |  0,15    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | makaron           | 2   eded |  2       |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | baliq konservasi  | 2   eded |  3,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | lavas             | 1   eded |  1,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | corek             | 2   eded |  0,50    |18 % |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | pomidor           | 2   kq   |  2,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | xiyar             | 2   kq   |  2       |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90              |
+             *  | toyuq             | 2   eded |  2,50    |0 %  |      |
+             *   Sizin qazanciniz=================== 0,90
+             * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+             * Endirim                                          | 2,48 |
+             * vergi edv                                        | 5,02 |
+             * yekun mebleg                                     | 54,50|
+             * odenis novu: kartla odenib                        
+             * tarix 20:04:21
+             * qebz nomresi : 12
+             *
+             * Musterinin hesabinda umumi 450 azn pul var,
+             * elave olaraq musteri bravo endirim kartina sahib oldugu ucun her alis verishinde
+             * umumi meblegin 2% qeder endirim elde edir. Musteri odenisi kartla ederse 0,18 elave deyer vergisinin
+             * 10 % qederini , negd pulla ederse 15 % qederini yeniden musterinin hesabina kocurulur.
+             *
+             * sizin qazanciniz :
+             * | Un                | 5 kq     |  1       |0 %  | 5
+             *  Sizin qazanciniz=================== 0,495 ( 5 % endirim olub bu mehsulda digerlerinde de bu sekilde endrim olarsa hesablanmalidir.)
+             *  0,495 qaliqlarda yuvarlasdirmada edersiz 
+             * EDV :
+             *  asagida gosterildiyi kimi bezi mehsullarda 0% bezilerinde 18% edv olmalidir.
+             *  Sonda elave deyer vergisi-de hansi mehsullarda varsa toplanib yekun meblegin
+             *  uzerine gelmelidir. Musteri odenisi kart-la  edibse 10% negd yolla odenis edibse
+             *  15 % umumi edv-den musterinin hesabina pul qayidacaq.
+             * 
+             *  | mehsul adi        |  miqdar  |  qiymet  |EDV  |toplam
+             *  | Un                | 5 kq     |  1       |0 %  |5
+             *   Sizin qazanciniz=================== 0,495
+             *  | quzu eti          | 3,5 kq   |  12      |18 % |42
+             *
+             *
+             *  alis veris 15 azn den azdirsa catdirilma ucun 4,50 azn teleb olunacaq.
+             */
+
+
+            #endregion
+            #region
+            Console.WriteLine(" Hesabinizda olan mebleg  450 AZN\n");
+            Console.WriteLine(" Product Menu ---------------------------\n");
+
+            Console.WriteLine("| mehsul adi        |   qiymet    |EDV |\n");
+            Console.WriteLine("| Un                |   1  AZN    |5 %  |");
+
+            Console.WriteLine("| quzu eti          |   12 AZN    |18 % |");
+
+            Console.WriteLine("| cay               |   1,50 AZN  |2 %  |");
+
+            Console.WriteLine("| alma qirmizi      |   2,50 AZN  |0 %  |");
+
+            Console.WriteLine("| alma sari         |   2 AZN     |18 % |");
+
+            Console.WriteLine("| goyerti           |   0,15 AZN  |0 %  |");
+
+            Console.WriteLine("| makaron           |   2  AZN    |7 %  |");
+
+            Console.WriteLine("| baliq konservasi  |   3,50 AZN  |0 %  |");
+
+            Console.WriteLine("| lavas             |   1,50 AZN  |0 %  |");
+
+            Console.WriteLine("| corek             |   0,50 AZN  |18 % |");
+
+            Console.WriteLine("| pomidor           |   2,50 AZN  |0 %  |");
+
+            Console.WriteLine("| xiyar             |   2  AZN    |6 %  |");
+                     
+            Console.WriteLine("| toyuq             |   2,50 AZN  |0 % |\n\n");
+            Random random = new Random();
+            Productcs product = new Productcs()           
+            {
+                Name = "Un",
+                Miqdar = 1,
+                Qiymet = 2,
+                Edv = 0.5,
+                Odenis = "Kartla",
+                BravoKart = "var"
+            };
+           
+
+            Console.WriteLine($"Elave odenilecek Mebleg : {(product.Miqdar * product.Qiymet) * product.Edv}");
+            if (Productcs.total < 15)
+            {
+                Console.WriteLine("Elave catdirilma ucun : 4.50 AZN ");
+                Console.WriteLine($"Yekun mebleg : {Productcs.total + 4.50}");
+            }
+            else
+            {
+                Console.WriteLine($"Yekun mebleg : {Productcs.total}");
+            }
+            Console.WriteLine($"Odenis novu: {product.Odenis}");
+            Console.WriteLine($"Tarix : {DateTime.Now}");
+            Console.WriteLine($"Qebzin omresi : {random.Next(1, 100000)}");
             #endregion
         }
     }
